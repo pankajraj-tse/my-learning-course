a3f235cd7dddc980fdb9ea2773a72300e5be923f
Pankaj Raj <pankajraj25dec@gmail.com>
2024-11-21 14:21:17 +0000

    this is my 2nd commit

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-# this is thr bash scipt which we can use
+# this is thr bash scipt which we can 
+# I have learned how to push the changes to git
+# this is my 2nd example
+# if it works fine then I will check how to pull the request and then move the 
+# changes again to git hub
+# this is very interesting 
+# i am happy to learn this thins from you tube and
+# documents
 #
